0128e629ebe52d001b9905cf678294152ec5615c
Zubaer <zubaerr@Zubaers-Air.communityfibre.co.uk>
2026-03-21 17:00:51 +0000

    Initial Spring Boot project

---
+++ src/main/java/com/example/aedea/AedeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aedea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AedeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AedeaApplication.class, args);
+	}
+
+}
